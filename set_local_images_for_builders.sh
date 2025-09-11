5d55734dbcf891cf1000c8155209aa85d86b7047
Costas Papastathis <papastathiscr@gmail.com>
2025-09-11 16:04:57 +0200

    fix: updating jam, buildpacks .vscode files

--- set_local_images_for_builders.sh
+++ set_local_images_for_builders.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
 
+docker pull docker.io/paketobuildpacks/builder-jammy-base
+docker tag docker.io/paketobuildpacks/builder-jammy-base localhost:5000/paketobuildpacks/builder-jammy-base
+docker push localhost:5000/paketobuildpacks/builder-jammy-base
+
+docker pull docker.io/paketobuildpacks/node-start
+docker tag docker.io/paketobuildpacks/node-start localhost:5000/paketobuildpacks/node-start
+docker push localhost:5000/paketobuildpacks/node-start
+
+exit 0
+
 docker pull docker.io/paketobuildpacks/build-jammy-base:0.1.143
 docker tag docker.io/paketobuildpacks/build-jammy-base:0.1.143 localhost:5000/paketobuildpacks/build-jammy-base:0.1.143
 docker tag docker.io/paketobuildpacks/build-jammy-base:0.1.143 localhost:5000/paketobuildpacks/build-jammy-base:latest
@@ -23,3 +33,11 @@ docker tag docker.io/paketobuildpacks/build-ubi8-base:0.0.146 localhost:5000/pak
 docker tag docker.io/paketobuildpacks/build-ubi8-base:0.0.146 localhost:5000/paketobuildpacks/build-ubi8-base:latest
 docker push localhost:5000/paketobuildpacks/build-ubi8-base:0.0.146
 docker push localhost:5000/paketobuildpacks/build-ubi8-base:latest
+
+docker pull docker.io/paketobuildpacks/build-jammy-base:0.1.143
+docker tag docker.io/paketobuildpacks/build-jammy-base:0.1.143 localhost:5000/paketobuildpacks/build-jammy-base:0.1.143
+docker tag docker.io/paketobuildpacks/build-jammy-base:0.1.143 localhost:5000/paketobuildpacks/build-jammy-base:latest
+docker push localhost:5000/paketobuildpacks/build-jammy-base:0.1.143
+docker push localhost:5000/paketobuildpacks/build-jammy-base:latest
+
+
